6d519de07206a9703e670e9523e83bbb4dc4bcae
oPhamThiDao <pham.thi.dao@sun-asterisk.com>
2020-08-10 18:41:30 +0700

    fix error when send email was nil (#3)

--- app/controllers/recipients_controller.rb
+++ app/controllers/recipients_controller.rb
@@ -3,6 +3,7 @@ class RecipientsController < ApplicationController
   end
 
   def create
+    @index = nil
     begin
       # create file
       file_path = save_tempfile(params[:file_csv])
@@ -11,12 +12,18 @@ class RecipientsController < ApplicationController
       worksheet = workbook.worksheets[0]
       worksheet.each_with_index do |row, i|
         next if i == 0
-        UserMailer.welcome_email(row.cells.map(&:value)).deliver_later
+        @index = i + 1
+        values = row.cells.map { |c| c.try(:value) }
+        check_params!(values)
+        UserMailer.welcome_email(values).deliver_later
       end
     ensure
       # delete temp file
       File.unlink file_path if File.exist? file_path
     end
+    redirect_to recipients_path
+  rescue => e
+    flash.alert = "[Line - #{@index}] Error: #{e.message}"
     redirect_to recipients_path
   end
 
@@ -28,5 +35,19 @@ class RecipientsController < ApplicationController
       f.write(file.read)
     }
     return name
+  end
+
+  def check_params!(values)
+    errors = []
+    indexs = [0, 2, 7, 14]
+    titles = ["Email", "Họ và tên", "Lương", "Tổng"]
+    indexs.each do |id|
+      if values[id].blank?
+        errors << titles[indexs.index(id)]
+      end
+    end
+    if errors.present?
+      raise "Bạn chưa điền thông tin cho cột #{errors.join(", ")}"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  root "recipients#index"
   resources :recipients, only: %i(index create)
 end
