5df8c70b1119a47b7a85b40a431c0027d5cda92b
oPhamThiDao <pham.thi.dao@sun-asterisk.com>
2020-11-10 11:34:50 +0700

    edit content mail

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -8,7 +8,7 @@ class UserMailerPreview < ActionMailer::Preview
 
   def salary
     receiver_id = 2
-    info = ["daopt22@gmail.com", 11, "Phạm Thị Đào", "CS1", "Sale ONL", 29, 28, 8000, nil, nil, nil, nil, nil, nil, 8285.71]
+    info = ["daopt22@gmail.com", 11, "Phạm Thị Đào", "CS1", "Sale ONL", 29, 28, 8000, nil, nil, 1888, nil, nil, nil, nil, 8285.71]
     UserMailer.salary(receiver_id, info).deliver_now
   end
 end
