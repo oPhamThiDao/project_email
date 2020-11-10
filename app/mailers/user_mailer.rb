8bf763b2c7754e82c2b9ad8446de8d8c331b855c
oPhamThiDao <pham.thi.dao@sun-asterisk.com>
2020-11-10 13:21:51 +0700

    add temp (#7)
    * add temp
    
    * add temp

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,4 @@
 class UserMailer < ApplicationMailer
-  def welcome_email(receiver_id, info)
-    email = info[0]
-    @info = info.drop(0)
-    ReceiverInfo.find_by(id: receiver_id)&.update(status: true)
-    mail(to: email, subject: "Lương #{1.month.ago.strftime("T%m/%Y")}")
-  end
-
   def salary(receiver_id, info)
     email = info[0]
     @info = info.drop(0)
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,11 +1,5 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-  def welcome_email
-    receiver_id = 2
-    info = ["daopt22@gmail.com", 11, "Phạm Thị Đào", "CS1", "Sale ONL", 29, 28, 8000, nil, nil, nil, nil, nil, nil, 8285.71]
-    UserMailer.welcome_email(receiver_id, info).deliver_now
-  end
-
   def salary
     receiver_id = 2
     info = ["daopt22@gmail.com", 11, "Phạm Thị Đào", "CS1", "Sale ONL", 29, 28, 8000, nil, nil, 1888, nil, nil, nil, nil, 8285.71]
